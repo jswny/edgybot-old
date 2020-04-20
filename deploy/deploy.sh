b29d5535ae34f0597ae6c444922ac40a8d449e32
Joe Sweeney <joe@j1.io>
2020-04-20 04:23:08 -0400

    Resolve Shellcheck warnings

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,12 +7,12 @@ else
   DEPLOY_VARIANT='stable'
 fi
 
-DISCORD_TOKEN_VARIABLE=$(echo "DISCORD_TOKEN_${DEPLOY_VARIANT}" | tr [:lower:] [:upper:])
+DISCORD_TOKEN_VARIABLE=$(echo "DISCORD_TOKEN_${DEPLOY_VARIANT}" | tr '[:lower:]' '[:upper:]')
 DISCORD_TOKEN="${!DISCORD_TOKEN_VARIABLE}"
 
 if [ -z "$DISCORD_TOKEN" ]
 then
-  echo '$DISCORD_TOKEN is empty! Exiting...'
+  echo 'DISCORD_TOKEN is empty! Exiting...'
   exit 1
 fi
 
--- deploy/setup_ssh.sh
+++ deploy/setup_ssh.sh
@@ -3,7 +3,10 @@
 DEPLOY_KEY_VARIABLE=$(echo "${DEPLOY_KEY_ENV_VAR_PREFIX}_key")
 DEPLOY_IV_VARIABLE=$(echo "${DEPLOY_KEY_ENV_VAR_PREFIX}_iv")
 
-openssl aes-256-cbc -K ${!DEPLOY_KEY_VARIABLE} -iv ${!DEPLOY_IV_VARIABLE} -in "${DEPLOY_ARTIFACTS_DIR}"/deploy_key.enc -out "${DEPLOY_ARTIFACTS_DIR}"/deploy_key -d
+openssl aes-256-cbc -K "${!DEPLOY_KEY_VARIABLE}" -iv "${!DEPLOY_IV_VARIABLE}" -in "${DEPLOY_ARTIFACTS_DIR}"/deploy_key.enc -out "${DEPLOY_ARTIFACTS_DIR}"/deploy_key -d
+
 chmod 600 "${DEPLOY_ARTIFACTS_DIR}"/deploy_key
+
 ssh-add "${DEPLOY_ARTIFACTS_DIR}"/deploy_key
-ssh-keyscan -t "$TRAVIS_SSH_KEY_TYPES" -H "$DEPLOY_SERVER" 2>&1 >> "${TRAVIS_HOME}"/.ssh/known_hosts
+
+{ ssh-keyscan -t "$TRAVIS_SSH_KEY_TYPES" -H "$DEPLOY_SERVER" >> "${TRAVIS_HOME}"/.ssh/known_hosts; } 2>&1
