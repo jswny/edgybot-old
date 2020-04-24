4fdc3e7725cc8de40e329995414ef313b0657861
Joe Sweeney <joe@j1.io>
2020-04-24 15:08:12 -0400

    Remove old deploy scripts

--- deploy/docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build --tag "$REPOSITORY" .
-docker tag "$REPOSITORY" "$DOCKER_IMAGE_NAME"
-docker push "$DOCKER_IMAGE_NAME"
