f8a1e4aa48f628bf19bc4ce47c3fa6dcd8a25156
Joe Sweeney <joe@j1.io>
2020-04-27 16:21:09 -0400

    Setup Ecto repo

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,13 @@
 import Config
 
+config :edgybot, Edgybot.Repo,
+  database: "edgybot_repo",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
+config :edgybot, ecto_repos: [Edgybot.Repo]
+
 config :nostrum,
   token: System.get_env("DISCORD_TOKEN"),
   num_shards: :auto
