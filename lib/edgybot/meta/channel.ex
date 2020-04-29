b637b05a3b487c33f1ed5aee367db7a04b4a02bd
Joe Sweeney <joe@j1.io>
2020-04-29 12:45:35 -0400

    Validate snowflakes

--- lib/edgybot/meta/channel.ex
+++ lib/edgybot/meta/channel.ex
@@ -2,6 +2,7 @@ defmodule Edgybot.Meta.Channel do
   use Ecto.Schema
   import Ecto.Changeset
   alias Edgybot.Meta.Server
+  import Edgybot.Meta.Snowflake
 
   schema "channels" do
     field :snowflake, :integer
@@ -15,6 +16,7 @@ defmodule Edgybot.Meta.Channel do
     channel
     |> cast(params, [:snowflake, :name, :server_id])
     |> validate_required([:snowflake, :name, :server_id])
+    |> validate_snowflake(:snowflake)
     |> assoc_constraint(:server)
     |> unique_constraint(:snowflake)
   end
--- lib/edgybot/meta/server.ex
+++ lib/edgybot/meta/server.ex
@@ -1,6 +1,7 @@
 defmodule Edgybot.Meta.Server do
   use Ecto.Schema
   import Ecto.Changeset
+  import Edgybot.Meta.Snowflake
 
   schema "servers" do
     field :snowflake, :integer
@@ -14,6 +15,7 @@ defmodule Edgybot.Meta.Server do
     server
     |> cast(params, [:snowflake, :name, :active])
     |> validate_required([:snowflake, :name, :active])
+    |> validate_snowflake(:snowflake)
     |> unique_constraint(:snowflake)
   end
 end
