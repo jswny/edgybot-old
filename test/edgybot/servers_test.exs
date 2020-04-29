c2b905dc4715ff1b139c9c7872f6e5bd9e9edb7e
Joe Sweeney <joe@j1.io>
2020-04-29 02:42:48 -0400

    Add more server tests

--- test/edgybot/servers_test.exs
+++ test/edgybot/servers_test.exs
@@ -2,16 +2,30 @@ defmodule Edgybot.ServersTest do
   use Edgybot.RepoCase
 
   alias Edgybot.Meta
-  alias Edgybot.Meta.Server
 
   @valid_attrs %{
     discord_id: "123",
     name: "foo"
   }
 
+  @invalid_attrs %{
+    discord_id: "123",
+  }
+
   describe "servers" do
+    alias Edgybot.Meta.Server
+
     test "create_server/1 with valid data creates a server" do
       assert {:ok, %Server{}} = Meta.create_server(@valid_attrs)
+    end
+
+    test "create_server/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Meta.create_server(@invalid_attrs)
+    end
+
+    test "create_server/1 with existing discord_id returns error changeset" do
+      Meta.create_server(@valid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Meta.create_server(@valid_attrs)
     end
   end
 end
